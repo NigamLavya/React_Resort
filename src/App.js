04a3225f1637840b586172daa656739e0f0bc4d9
Lavya Nigam <lavya.nigam@oattravel.com>
2020-12-29 22:52:42 +0530

    added pages with react router dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import './App.css';
+import {
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Home from './pages/Home'
+import Rooms from './pages/Rooms'
+import SingleRoom from './pages/SingleRoom'
+import Error from './pages/Error'
 
 function App() {
-  return <>Hello from react</>;
+  return( <>
+  <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/rooms">
+            <Rooms />
+          </Route>
+          <Route exact path="/rooms/:id" component={SingleRoom}>
+            <SingleRoom />
+          </Route>
+          <Route exact path="*">
+            <Error />
+          </Route>
+        </Switch>
+  </>);
 }
 
 export default App;
---
+++ src/pages/Rooms.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Rooms = () => {
+    return (
+        <div>
+            Rooms content
+        </div>
+    )
+}
+
+export default Rooms
