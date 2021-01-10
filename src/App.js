af61369c5dad6549c9a3b55651a351fb7c1af008
Lavya Nigam <lavya.nigam@oattravel.com>
2021-01-10 17:39:58 +0530

    added navbar

--- src/App.js
+++ src/App.js
@@ -2,31 +2,34 @@ import React from 'react';
 import './App.css';
 import {
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
 import Home from './pages/Home'
 import Rooms from './pages/Rooms'
 import SingleRoom from './pages/SingleRoom'
 import Error from './pages/Error'
+import Navbar from './components/Navbar'
 
 function App() {
-  return( <>
-  <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/rooms">
-            <Rooms />
-          </Route>
-          <Route exact path="/rooms/:id" component={SingleRoom}>
-            <SingleRoom />
-          </Route>
-          <Route exact path="*">
-            <Error />
-          </Route>
-        </Switch>
-  </>);
+  return (
+  <>
+    <Navbar/>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/rooms">
+        <Rooms />
+      </Route>
+      <Route exact path="/rooms/:id" component={SingleRoom}>
+        <SingleRoom />
+      </Route>
+      <Route exact path="*">
+        <Error />
+      </Route>
+    </Switch>
+  </>
+  )
 }
 
 export default App;
