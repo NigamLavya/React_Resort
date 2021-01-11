c35f385fd026afdd3c72c74cd6dac2d42ae1468b
Lavya Nigam <lavya.nigam@oattravel.com>
2021-01-11 18:50:20 +0530

    Added context API to the project

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import {
   BrowserRouter as Router
 } from "react-router-dom";
 import App from './App';
+import { RoomProvider } from './Context'
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <RoomProvider>
+    <Router>
+      <App />
+    </Router>
+  </RoomProvider>,
   document.getElementById('root')
 );
