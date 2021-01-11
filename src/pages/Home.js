cf584abd489071b1dc731049e3d183a3a4af5a51
Lavya Nigam <lavya.nigam@oattravel.com>
2021-01-11 15:01:07 +0530

    hero banner addded

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Banner from '../components/Banner'
+import { Link } from 'react-router-dom'
+
 
 const Home = () => {
     return (
-        <div>
-            Home content
-        </div>
+        <Hero><Banner title="Luxurios Rooms" subtitle="deluxe rooms starting at $299"><Link to="/rooms" className="btn-primary">Our Rooms</Link> </Banner> </Hero>
     )
 }
 export default Home;
